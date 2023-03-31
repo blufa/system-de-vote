fc21b1b1b7b0500efce11d7a1b4562e6a137a62e
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-03-31 13:49:14 -0500

    Default spring-boot project added

---
+++ server-voting-system/src/main/java/com/senatic/servervotingsystem/ServerVotingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.senatic.servervotingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerVotingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerVotingSystemApplication.class, args);
+	}
+
+}
---
+++ server-voting-system/src/test/java/com/senatic/servervotingsystem/ServerVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senatic.servervotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
