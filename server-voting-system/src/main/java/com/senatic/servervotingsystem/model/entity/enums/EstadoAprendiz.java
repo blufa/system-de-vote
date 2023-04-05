3344f129c69f84b2726bcff5051d3fedebf9481b
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-05 12:53:06 -0500

    Changing implementation

--- server-voting-system/src/main/java/com/senatic/servervotingsystem/model/entity/enums/EstadoAprendiz.java
+++ server-voting-system/src/main/java/com/senatic/servervotingsystem/model/entity/enums/EstadoAprendiz.java
@@ -26,25 +26,12 @@ public enum EstadoAprendiz {
     } 
 
     public static EstadoAprendiz checkAndAssign(String status){
-
         EstadoAprendiz estadoAssigned = EstadoAprendiz.NOT_PROVIDED;
-
-        if (EstadoAprendiz.EN_FORMACION.plainText.equals(status)) {
-            estadoAssigned = EstadoAprendiz.EN_FORMACION;
-        } else if (EstadoAprendiz.CANCELADO.plainText.equals(status)) {
-            estadoAssigned = EstadoAprendiz.EN_FORMACION;
-        } else if (EstadoAprendiz.CANCELADO.plainText.equals(status)) {
-            estadoAssigned = EstadoAprendiz.EN_FORMACION;
-        } else if (EstadoAprendiz.RETIRADO.plainText.equals(status)) {
-            estadoAssigned = EstadoAprendiz.RETIRADO;
-        } else if (EstadoAprendiz.RETIRO_VOLUNTARIO.plainText.equals(status)) {
-            estadoAssigned = EstadoAprendiz.RETIRO_VOLUNTARIO;
-        } else if (EstadoAprendiz.TRASLADADO.plainText.equals(status)) {
-            estadoAssigned = EstadoAprendiz.TRASLADADO;
-        } else if (EstadoAprendiz.SUSPENDIDO.plainText.equals(status)) {
-            estadoAssigned = EstadoAprendiz.SUSPENDIDO;
+        for ( EstadoAprendiz estado : EstadoAprendiz.values()) {
+            if (estado.plainText.equals(status)) {
+                estadoAssigned = estado;
+            }
         }
-
         return estadoAssigned;
     }
 
