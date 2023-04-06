062ae1771bc7da14cd047008b03141827358c430
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-06 14:11:05 -0500

    Getting back the mandatory photo for candidato

--- server-voting-system/src/main/java/com/senatic/servervotingsystem/model/mapper/CandidatoMapper.java
+++ server-voting-system/src/main/java/com/senatic/servervotingsystem/model/mapper/CandidatoMapper.java
@@ -1,10 +1,8 @@
 package com.senatic.servervotingsystem.model.mapper;
 
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.senatic.servervotingsystem.controller.exceptionHandler.exception.EntityNotFoundException;
 import com.senatic.servervotingsystem.model.dto.CandidatoDTO;
 import com.senatic.servervotingsystem.model.entity.Aprendiz;
 import com.senatic.servervotingsystem.model.entity.Candidato;
@@ -12,7 +10,6 @@ import com.senatic.servervotingsystem.model.entity.Imagen;
 import com.senatic.servervotingsystem.model.entity.Votacion;
 import com.senatic.servervotingsystem.model.entity.enums.EstadoCandidato;
 import com.senatic.servervotingsystem.service.AprendicesService;
-import com.senatic.servervotingsystem.service.ImagenesService;
 import com.senatic.servervotingsystem.service.VotacionesService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,25 +19,13 @@ import lombok.RequiredArgsConstructor;
 public class CandidatoMapper implements GenericMapper<Candidato, CandidatoDTO> {
 
     private final AprendicesService aprendicesService;
-    private final ImagenesService imagenesService;
     private final VotacionesService votacionesService;
 
     @Override
     public Candidato dtoToPojo(CandidatoDTO dto) {
         Aprendiz aprendiz = aprendicesService.findById(dto.getDocumento()).get();
         Votacion votacion = votacionesService.getVotacionById(dto.getIdVotacion()).get();
-        Imagen imagen = Imagen.builder().build();
-        if (dto.getImagen() == null) {
-            Optional<Imagen> optionalImagen = imagenesService.getImagenById(dto.getDocumento());
-            if (optionalImagen.isPresent()) {
-                imagen = optionalImagen.get();
-            } else {
-                throw new EntityNotFoundException("IMAGE not found");
-            }
-        } else {
-            imagen.setId(dto.getDocumento());
-            imagen.setImage(dto.getImagen());
-        }
+        Imagen imagen = Imagen.builder().id(dto.getDocumento()).image(dto.getImagen()).build();
         Candidato candidato = Candidato.builder()
                 .aprendiz(aprendiz)
                 .imagen(imagen)
@@ -56,7 +41,6 @@ public class CandidatoMapper implements GenericMapper<Candidato, CandidatoDTO> {
 
     @Override
     public CandidatoDTO pojoToDto(Candidato pojo) {
-        //MultipartFile multipartFile = imagenMapper.pojoToDto(pojo.getImagen());
         CandidatoDTO candidatoDTO = CandidatoDTO.builder()
                 .documento(pojo.getAprendiz().getId())
                 .imagen(pojo.getImagen().getImage())
