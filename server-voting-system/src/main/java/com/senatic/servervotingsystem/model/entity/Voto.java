91d09158b91b07b05bb8017166be6afb7fbdc9dd
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-06 18:27:16 -0500

    Unworking unique index at voto entity

--- server-voting-system/src/main/java/com/senatic/servervotingsystem/model/entity/Voto.java
+++ server-voting-system/src/main/java/com/senatic/servervotingsystem/model/entity/Voto.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -23,7 +22,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="votos", indexes = @Index(name="votos_unique", columnList = "idVotacion, idAprendiz", unique = true))
+@Table(name="votos")
 public class Voto {
     
     @Id
